23a67c265ca4ae0e5f182879b91f94bd15a5d7a8
Grace Colette <ilovegrace09@gmail.com>
2019-06-22 23:51:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  season.each do |season, holiday|
+  holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supply| supply == "BBQ"
       puts holiday
   end
